360a9622022379e484aa58329a23b610fc153d77
Mikolaj Figurski <meek.f80@gmail.com>
2020-04-13 01:22:19 -0400

    Trie functionality

--- Trie.java
+++ Trie.java
@@ -1,34 +1,88 @@
 import java.util.*;
+import java.io.File;
+
 public class Trie {
     public static class TrieNode {
         //each child of a TrieNode corresponds to a certain letter
         private HashMap<Character, TrieNode> charMap = new HashMap<>();
-        /*
-            There is more than one way to implement the trie. A 
-            recursive approach might have the outer Trie.insert and 
-            Trie.contains methods call the following recursive methods:
-            
-            public void insert(String s){
+
+        public void insert(String s) {
+            // exit condition
+            if (s.length() <= 0) {
+                charMap.put('\0', null);
+                return;
             }
-            public boolean contains(String s){
+
+            char c = s.charAt(0);
+            TrieNode child = charMap.get(c);
+            if (child == null) { // actually need to insert
+                child = new TrieNode();
+                charMap.put(c, child); // save
             }
-        */
+            child.insert(s.substring(1, s.length()));
+        }
+
+        public boolean contains(String s) {
+            if (s.length() <= 0 && charMap.containsKey('\0'))
+                return true;
+            if (charMap.containsKey(s.charAt(0)))
+                return charMap.get(s.charAt(0)).contains(s.substring(1, s.length()));
+            return false;
+        }
+
+        public String toString() {
+            String ret = "(";
+            for (char key : charMap.keySet()) {
+                if (key == '\0') {
+                    ret += '.';
+                } else {
+                    ret += key;
+                    ret += charMap.get(key).toString();
+                }
+            }
+            ret += ")";
+            return ret;
+       }
     }
     public TrieNode root = new TrieNode();
     public void insert(String s){
-        // your implementation goes here
+        root.insert(s.toLowerCase());
     }
     public boolean contains(String s){
-        // your implementation goes here
-        
-        // warning: tries normally need the list of words to be 
-        //          prefix-free
-        return true;
+        return root.contains(s.toLowerCase());
     }
-    
+
     //given a file with one word per line, insert all the words in the
     //file into the trie
-    public void insertDictionary(String filename){
-        // your implementation goes here
+    public void insertDictionary(String filename) throws Exception {
+        File f = new File(filename);
+        Scanner sc = new Scanner(f);
+
+        while (sc.hasNext()) {
+            root.insert(sc.next());
+        }
+
     }
+
+    public String toString() {
+        return root.toString();
+    }
+
+
+    public static void main(String[] args) throws Exception {
+        System.out.println("> Trie.java");
+        Trie t = new Trie();
+        t.insertDictionary("./words.txt");
+
+        // t.insert("hello");
+        // t.insert("hellor");
+        // t.insert("hell");
+        // t.insert("helapenos");
+        // System.out.println(t.contains("hello"));
+        // System.out.println(t.contains("hell"));
+        // System.out.println(t.contains("hahah"));
+        
+        System.out.println(t);
+    }
+
 }
--- WordSplitter.java
+++ WordSplitter.java
@@ -1,9 +1,12 @@
 import java.util.HashSet;
+
 public class WordSplitter {
-    public static void main(String[] args){
+    public static void main(String[] args) throws FileNotFoundException {
+        Scanner file = new Scanner(new File("./words.txt"));
+        System.out.println(file.next());
         // Consider using the following if you haven't done Trie.java yet:
         // HashSet<String> dictionary;
-        
+
         // your implementation goes here
     }
 }
